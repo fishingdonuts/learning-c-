5641e59219bc0756210c7eb040aaa803cf0f85f3
Yousuf <67607935+fishingdonuts@users.noreply.github.com>
2021-02-03 12:28:59 +0530

    Add files via upload

---
+++ c_operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int a = 10, b = 20, c;
+c=a&b;
+printf("Output for c = a&b = %d\n", c);
+
+
+c=a|b;
+printf("Output for c  = a|b = %d", c);
+return 0;
+
+/* The output is in binary*/ 
+
+}
